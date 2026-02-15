f3ca9a5fc352acd489cefc5de411c0811f6c1622
badhon pimjo <badhon.pimjo@gmail.com>
2026-02-15 12:57:10 +0600

    add api documentatin

--- src/auth/dot/auth.dto.ts
+++ src/auth/dot/auth.dto.ts
@@ -1,9 +1,17 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class AuthPayloadDto {
   @IsEmail()
+  // @ApiProperty({
+  //   description: 'The email of the user',
+  //   example: 'user@example.com',
+  // })
   username: string;
 
+  // @ApiProperty({
+  //   description: 'The password of the user',
+  //   example: 'password123',
+  // })
   @IsString()
   @IsNotEmpty()
   password: string;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('My API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,4 +24,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 void bootstrap();
